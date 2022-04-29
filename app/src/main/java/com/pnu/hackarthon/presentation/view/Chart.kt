61bf7917e49467f974dd23613ce0a272173ef583
홍유준 <hyjhyj0901@pusan.ac.kr>
2022-04-30 04:37:29 +0900

    Chart Data Visualization

--- app/src/main/java/com/pnu/hackarthon/presentation/view/Chart.kt
+++ app/src/main/java/com/pnu/hackarthon/presentation/view/Chart.kt
@@ -3,6 +3,9 @@ package com.pnu.hackarthon.presentation.view
 import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.input.key.Key.Companion.D
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.PieData
@@ -12,31 +15,30 @@ import com.github.mikephil.charting.utils.ColorTemplate
 import com.pnu.hackarthon.R
 import com.pnu.hackarthon.domain.model.TaskLog
 import com.pnu.hackarthon.domain.repository.TaskLogRepository
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
-class Chart @Inject constructor(
-    private val repository: TaskLogRepository
-) : Activity() {
+@AndroidEntryPoint
+class Chart : ComponentActivity() {
     private var allTasks: List<TaskLog> = emptyList<TaskLog>()
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            repository.getAllTasks().collect {
-                allTasks = it
-            }
-        }
-    }
     val chart by lazy { findViewById<com.github.mikephil.charting.charts.PieChart>(R.id.pieChart) }
+    @Inject
+    lateinit var repository : TaskLogRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.data_chart)
 
-        chart.run {
-
+        runBlocking {
+            allTasks = repository.getAllTasks().first()
         }
+
         with(chart) {
             setUsePercentValues(true)
             description.isEnabled = false
@@ -47,28 +49,28 @@ class Chart @Inject constructor(
 
             val yValues: ArrayList<PieEntry> = ArrayList()
             with(yValues) {
-                add(PieEntry(1.0F))
-                add(PieEntry(1.0F))
-                add(PieEntry(1.0F))
-                add(PieEntry(1.0F))
-                add(PieEntry(1.0F))
+                add(PieEntry(allTasks.count { it -> it.title == "분유" }.toFloat(), "분유"))
+                add(PieEntry(allTasks.count { it -> it.title == "기저귀" }.toFloat(), "기저귀"))
+                add(PieEntry(allTasks.count { it -> it.title == "수면" }.toFloat(), "수면"))
+                add(PieEntry(allTasks.count { it -> it.title == "이유식" }.toFloat(), "이유식"))
             }
 
             animateY(1000, Easing.EaseInOutCubic)
 
             val dataSet: PieDataSet = PieDataSet(yValues, "")
             with(dataSet) {
-                sliceSpace = 3f
-                selectionShift = 5f
+                sliceSpace = 4f
+                selectionShift = 15f
                 setColors(*ColorTemplate.JOYFUL_COLORS)
+
             }
 
             val pieData: PieData = PieData(dataSet)
             with(pieData) {
-                setValueTextSize(10f)
+                setValueTextSize(12f)
                 setValueTextColor(Color.BLACK)
                 val des = Description()
-                des.text = "도시별 확진자 상위 5곳"
+                des.text = "육아 액티비티 발생 비율"
                 description = des
             }
             data = pieData
