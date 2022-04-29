8e6610b534011714feb9e1f11e1b5b02af80e7d9
Won Suk <daily142857@gmail.com>
2022-04-29 12:26:14 +0900

    feat: 스플래시 화면 구현

--- app/src/main/java/com/pnu/hackarthon/presentation/view/Splash.kt
+++ app/src/main/java/com/pnu/hackarthon/presentation/view/Splash.kt
@@ -1,52 +1,75 @@
 package com.pnu.hackarthon.presentation.view
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.pnu.hackarthon._constant.UIConstant
 import com.pnu.hackarthon.presentation.navigation.Screen
 import com.pnu.hackarthon.ui.theme.HackarthonTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
     navController: NavController
 ) {
-    SplashBackground {
+    val duration = UIConstant.ANIMATION_DURATION_SPLASH
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(duration)
+    )
+    
+    LaunchedEffect(true) {
+        startAnimation = true
+
+        delay(duration + 500L)
+        navController.popBackStack()
+        navController.navigate(Screen.Home.route)
+    }
+
+    SplashBackground(alpha = alphaAnim.value) {
         SplashContent {
-            Text(
-                text = "스플래시 화면",
-                fontSize = 80.sp
-            )
-            Button(
-                onClick = {
-                    navController.navigate(route= Screen.Home.route) {
-                        launchSingleTop = true
-                    }
-                }
-            ) {
-                Text(text = "홈 화면으로!")
-            }
+            LogoArea()
         }
     }
 }
 
 @Composable
+private fun LogoArea() {
+    Spacer(Modifier.height(50.dp))
+    Text(
+        modifier = Modifier,
+        text = "HACKARTHON",
+        color = Color.Black,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
 private fun SplashBackground(
+    alpha: Float,
     content: @Composable BoxScope.() -> Unit,
 ) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.White),
+            .background(color = Color.White)
+            .alpha(alpha),
         content = content
     )
 }
